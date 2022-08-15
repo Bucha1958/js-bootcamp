8d2698b8acc664525e54bbfe621eb0df4e887e2d
Bucha1958 <okoriemmadu@gmail.com>
2022-08-15 23:52:06 +0100

    Function file

---
+++ js-bootcamp/convert_fun.js
@@ -0,0 +1,11 @@
+/**
+ * convertFahrenheitTocelsius - Function that convert fahrenheit to
+ * celsius
+ * @fahrenheit : Argument function passed 
+ * Return : It returns a variable declared in the function
+ */
+let convertFahrenheitToCelsius = function(fahrenheit)
+{
+    let celsius = 5/9 * (fahrenheit-32);
+    return celsius;
+}
